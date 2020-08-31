096102b374979273f49e2a5bca49b4272b2ad58a
sara <saronah1099@gmail.com>
2020-08-31 19:53:26 +0300

    moved elk to new repo

--- elk/elk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-helm repo add elastic https://helm.elastic.co
-helm repo add fluent https://fluent.github.io/helm-charts
-helm repo update
-helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=elk
-helm install fluent-bit fluent/fluent-bit --namespace=elk
-helm install kibana elastic/kibana --version=7.9.0 --namespace=elk --set service.type=NodePort
-kubectl apply -f ingress.yaml -n elk
