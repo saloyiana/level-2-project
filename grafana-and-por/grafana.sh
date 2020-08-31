4b77fa613fd4748218746eb64f2f3e4ba237eb7b
sara <saronah1099@gmail.com>
2020-08-31 19:54:49 +0300

    moved grafana to new repo

--- grafana-and-por/grafana.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-POD=$(kubectl get pods -n monitor -o json | jq '.items[1].metadata.name' | xargs)
-echo "grafana Port"
-echo port: $(kubectl get service prometheus-operator-grafana -n monitor -o json | jq '.spec.ports[0].nodePort')
--- grafana-and-por/readme.md
+++
@@ -1,4 +0,0 @@
-# Prometheus and Grafana
-## Together at last
-
-This is a trivial script that installs prometheus and grafana using helm. It exists for the DevOps Bootcamp that I'm running in SA.
