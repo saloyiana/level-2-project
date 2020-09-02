bc00f432f4cd7ebd2587a5d2e17cba6333c75c8e
Nawaf Abdullah <nawaf.manage@gmail.com>
2020-09-02 12:15:20 +0300

    modified index and Dockerfile

--- project-assessment-site/index.js
+++ project-assessment-site/index.js
@@ -2,7 +2,6 @@ var express = require("express");
 var app = express();
 
 app.use(express.static('public'))
-
 app.listen(80, () => {
     console.log("Project Assessment Website: running on port 80");
 });
